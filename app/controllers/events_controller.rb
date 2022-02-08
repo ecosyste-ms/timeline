4d62b8ceb098dda193485b2b5cd6cbfb971cc111
Andrew Nesbitt <andrewnez@gmail.com>
2022-02-08 15:29:55 +0000

    basic pagination

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,5 +2,8 @@ class EventsController < ApplicationController
   def show
     @repository = params[:id]
     @events = Event.where(repository: @repository).order('id DESC').limit(30) # TODO pagination
+    if params[:before]
+      @events = @events.where('events.id < ?', params[:before]).order('id DESC')
+    end
   end
 end
