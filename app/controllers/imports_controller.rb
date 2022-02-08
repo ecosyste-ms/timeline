ac176ed664ce73392a2837e0ea43b55938342631
Andrew Nesbitt <andrewnez@gmail.com>
2022-02-08 15:39:23 +0000

    show some recent events on the homepage

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -1,5 +1,6 @@
 class ImportsController < ApplicationController
   def index
     @imports = Import.order('created_at DESC').limit(24*7).map{|i| [i.datetime, i.event_count]}
+    @events = Event.order('id DESC').limit(30)
   end
 end
