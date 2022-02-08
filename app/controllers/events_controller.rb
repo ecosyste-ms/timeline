eef9b534a52553f2519657b0367a587717df049b
Andrew Nesbitt <andrewnez@gmail.com>
2022-02-08 15:39:03 +0000

    remove todo

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
   def show
     @repository = params[:id]
-    @events = Event.where(repository: @repository).order('id DESC').limit(30) # TODO pagination
+    @events = Event.where(repository: @repository).order('id DESC').limit(30)
     if params[:before]
       @events = @events.where('events.id < ?', params[:before]).order('id DESC')
     end
