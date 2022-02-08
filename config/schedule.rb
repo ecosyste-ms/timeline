aa064aa93343e28ae63a236e75e5af0ba92dcdf3
Andrew Nesbitt <andrewnez@gmail.com>
2022-02-08 16:37:52 +0000

    attempt imports a little more regularly

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 job_type :rbenv_rake, %Q{export PATH=/home/deploy/.rbenv/shims:/home/deploy/.rbenv/bin:/usr/bin:$PATH; eval "$(rbenv init -)"; \
   cd :path && RAILS_ENV=production bundle exec rake :task --silent :output }
 
-every 1.hour do
+every 15.minutes do
   rbenv_rake "events:last_day"
 end
