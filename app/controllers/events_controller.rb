169a4e68c1c6c7f171711d4bb08a0ef8eede3653
Andrew Nesbitt <andrewnez@gmail.com>
2022-02-08 13:53:41 +0000

    revert

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   def show
     @repository = params[:id]
-    @events = Event.where(repository: @repository).limit(30) # TODO pagination
+    @events = Event.where(repository: @repository).order('id DESC').limit(30) # TODO pagination
   end
 end
