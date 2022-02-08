c2ef709af64ec4feeec6d38b5a942b0b50210ebc
Andrew Nesbitt <andrewnez@gmail.com>
2022-02-08 15:43:17 +0000

    flip reverse it

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -1,6 +1,6 @@
 class ImportsController < ApplicationController
   def index
     @imports = Import.order('created_at DESC').limit(24*7).map{|i| [i.datetime, i.event_count]}
-    @events = Event.order('id DESC').limit(30).to_a.sort_by(&:created_at)
+    @events = Event.order('id DESC').limit(30).to_a.sort_by(&:created_at).reverse
   end
 end
