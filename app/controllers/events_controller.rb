c574fe68bfc6065763738dbc7ea12d3aa2e5db6a
Andrew Nesbitt <andrewnez@gmail.com>
2022-02-08 17:10:18 +0000

    page titles

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,7 @@
 class EventsController < ApplicationController
   def show
     @repository = params[:id]
+    @title = @repository
     @events = Event.where(repository: @repository).order('id DESC').limit(30)
     if params[:before]
       @events = @events.where('events.id < ?', params[:before]).order('id DESC')
