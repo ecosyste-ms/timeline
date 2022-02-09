667515b8854439c701e3915423363b035a9a60b8
Andrew Nesbitt <andrewnez@gmail.com>
2022-02-09 18:44:45 +0000

    todos

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,12 +2,15 @@ class EventsController < ApplicationController
   def show
     @repository = params[:id]
     @title = @repository
+
+    # TODO 404 if there are no events for repo at all
+
     @events = Event.where(repository: @repository).order('id DESC').limit(30)
 
     start_time = Time.now
     @events = @events.where('created_at between ? and ?', start_time - 1.month, start_time)
 
-    # TODO allow paging back before 1 month
+    # TODO allow paging back before 1 month (maybe by year?)
 
     if params[:before]
       @events = @events.where('events.id < ?', params[:before]).order('id DESC')
