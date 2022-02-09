5e915323583b06b6c32e50664cf6900d69194574
Andrew Nesbitt <andrewnez@gmail.com>
2022-02-09 15:11:17 +0000

    Limit events to most recent month

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,6 +3,10 @@ class EventsController < ApplicationController
     @repository = params[:id]
     @title = @repository
     @events = Event.where(repository: @repository).order('id DESC').limit(30)
+
+    start_time = Time.now
+    @events = @events.where('created_at between ? and ?', start_time - 1.month, start_time)
+
     if params[:before]
       @events = @events.where('events.id < ?', params[:before]).order('id DESC')
     end
