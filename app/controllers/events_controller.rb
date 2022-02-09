a9ae2c96592c9cecc56ca23cdc41f2b6bd76451c
Andrew Nesbitt <andrewnez@gmail.com>
2022-02-09 15:37:38 +0000

    try disabling time limit again

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,8 +4,8 @@ class EventsController < ApplicationController
     @title = @repository
     @events = Event.where(repository: @repository).order('id DESC').limit(30)
 
-    start_time = Time.now
-    @events = @events.where('created_at between ? and ?', start_time - 1.month, start_time)
+    # start_time = Time.now
+    # @events = @events.where('created_at between ? and ?', start_time - 1.month, start_time)
 
     if params[:before]
       @events = @events.where('events.id < ?', params[:before]).order('id DESC')
