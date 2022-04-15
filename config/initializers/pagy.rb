1e3e62b9782170d56852cf1f553cbd3ddebd9989
Andrew Nesbitt <andrewnez@gmail.com>
2022-04-15 11:07:24 +0100

    Document pagination params

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,4 +1,7 @@
 require 'pagy/extras/headers'
 require 'pagy/extras/countless'
+require 'pagy/extras/items'
 
 Pagy::DEFAULT[:items] = 100
+Pagy::DEFAULT[:items_param] = :per_page
+Pagy::DEFAULT[:max_items] = 100 
