27f26c9cfe3a89454874a11d836a1b9f59812ce1
Andrew Nesbitt <andrewnez@gmail.com>
2024-07-22 14:46:08 +0100

    Update pagy config

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,7 +1,9 @@
 require 'pagy/extras/headers'
+require 'pagy/extras/limit'
+require 'pagy/extras/bootstrap'
 require 'pagy/extras/countless'
-require 'pagy/extras/items'
+require 'pagy/extras/array'
 
-Pagy::DEFAULT[:items] = 100
-Pagy::DEFAULT[:items_param] = :per_page
-Pagy::DEFAULT[:max_items] = 1000 
+Pagy::DEFAULT[:limit] = 100
+Pagy::DEFAULT[:limit_param] = :per_page
+Pagy::DEFAULT[:limit_max] = 1000
