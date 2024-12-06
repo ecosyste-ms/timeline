d34abc5f3de3764ab27e89c8e3da9e625a6425d3
Andrew Nesbitt <andrewnez@gmail.com>
2024-12-06 12:51:44 +0000

    Add statement timeout configuration for actor index migration

--- db/migrate/20241206123035_add_actor_index_to_events.rb
+++ db/migrate/20241206123035_add_actor_index_to_events.rb
@@ -1,5 +1,7 @@
 class AddActorIndexToEvents < ActiveRecord::Migration[8.0]
   def change
+    ActiveRecord::Base.connection.execute("SET statement_timeout TO 0;") # No timeout
     add_index :events, :actor
+    ActiveRecord::Base.connection.execute("RESET statement_timeout;")
   end
 end
