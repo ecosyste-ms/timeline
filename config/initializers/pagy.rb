ed5275775ddc7d91df7ccb1ed709a227cee1bde8
Andrew Nesbitt <andrewnez@gmail.com>
2025-11-11 16:15:16 +0000

    update pagy config

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -3,7 +3,9 @@ require 'pagy/extras/limit'
 require 'pagy/extras/bootstrap'
 require 'pagy/extras/countless'
 require 'pagy/extras/array'
+require 'pagy/extras/overflow'
 
 Pagy::DEFAULT[:limit] = 100
 Pagy::DEFAULT[:limit_param] = :per_page
 Pagy::DEFAULT[:limit_max] = 1000
+Pagy::DEFAULT[:overflow] = :empty_page
