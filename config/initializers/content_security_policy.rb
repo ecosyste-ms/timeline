64b7fc12e4886ad6a691c6005094729c8b6953fb
Andrew Nesbitt <andrewnez@gmail.com>
2026-02-26 11:10:57 +0000

    Remove CSP hash that was causing unsafe-inline to be ignored
    The sha256 hash in script_src caused browsers to ignore unsafe-inline
    per the CSP spec, breaking rswag interactive docs.
    
    Fixes #824

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,7 +1,7 @@
 SecureHeaders::Configuration.default do |config|
   config.csp = {
     default_src: %w('self'),
-    script_src: %w('self' 'unsafe-inline' 'unsafe-hashes' 'sha256-pwQduWxX3Cm7lLbQmAos7n73RLP4QrsCDFejmsBdZ78=' https://cdnjs.cloudflare.com http://cdnjs.cloudflare.com https://cdn.jsdelivr.net http://cdn.jsdelivr.net https://unpkg.com http://unpkg.com https://static.cloudflareinsights.com http://static.cloudflareinsights.com),
+    script_src: %w('self' 'unsafe-inline' https://cdnjs.cloudflare.com http://cdnjs.cloudflare.com https://cdn.jsdelivr.net http://cdn.jsdelivr.net https://unpkg.com http://unpkg.com https://static.cloudflareinsights.com http://static.cloudflareinsights.com),
     style_src: %w('self' 'unsafe-inline' https://fonts.googleapis.com https://unpkg.com http://unpkg.com),
     font_src: %w('self' https://fonts.gstatic.com),
     img_src: %w('self' data: https:),
